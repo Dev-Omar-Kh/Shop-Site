a100ab70bcf1c27a2152f3b99e3b4b49bfc6f567
unknown <omar.er3434@gmail.com>
2024-07-09 16:41:15 +0300

    V 0.0.8

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,14 +1,92 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import sliderCss from './slider.module.css'
+import sliderCss from './slider.module.css';
+
+import fSlide from '../../images/first_slide.png';
+import sSlide from '../../images/second_slider.png';
+import tSlide from '../../images/third_slide.jpg';
 
 export default function Slider(){
+
+    
+    const [count, setCount] = useState(0);
+    const [fade, setFade] = useState(false);
+    const [manualChange, setManualChange] = useState(false);
+    const images = [fSlide , sSlide , tSlide];
+
+
+    useEffect(() => {
+
+        if (manualChange) return;
+
+        const interval = setInterval(() => {
+
+            setFade(true);
+
+            setTimeout(() => {
+
+                setCount((count + 1) % 3);
+                setFade(false);
+
+            }, 500);
+
+        }, 5000);
+
+        return () => clearInterval(interval);
+
+    } , [count , manualChange]);
+
+    const handlePrev = () => {
+
+        if (!fade) {
+
+            setManualChange(true);
+            setFade(true);
+
+            setTimeout(() => {
+
+                setCount((count - 1 + 3) % 3);
+                setFade(false);
+                setManualChange(false);
+
+            }, 500);
+
+        }
+
+    };
+
+    const handleNext = () => {
+
+        if (!fade) {
+
+            setManualChange(true);
+            setFade(true);
+
+            setTimeout(() => {
+
+                setCount((count + 1) % 3);
+                setFade(false);
+                setManualChange(false);
+
+            }, 500);
+
+        }
+
+    };
 
     return <React.Fragment>
 
         <div className={sliderCss.container}>
 
-            
+            <div onClick={handlePrev} className={sliderCss.arrows}><i className="fa-solid fa-chevron-left"></i></div>
+
+            <div className={sliderCss.slide_scroll}>
+
+                <img id='images' className={fade ? sliderCss.fade : sliderCss.fade_in} src={images[count]} alt="sliders" />
+
+            </div>
+
+            <div onClick={handleNext} className={sliderCss.arrows}><i className="fa-solid fa-chevron-right"></i></div>
 
         </div>
 
